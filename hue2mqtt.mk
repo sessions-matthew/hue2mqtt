3ab762f5cac495cefb3d89af18c58e0ea8b6f08e
sessions-matthew <sessions.matthew.j@gmail.com>
2024-12-26 14:55:13 -0800

    clean up buildroot install

--- hue2mqtt.mk
+++ hue2mqtt.mk
@@ -9,5 +9,6 @@ HUE2MQTT_SITE = ./package/hue2mqtt
 HUE2MQTT_SITE_METHOD = local# Other methods like git,wget,scp,file etc. are also available.
 HUE2MQTT_DEPENDENCIES = host-pkgconf dbus
 HUE2MQTT_INSTALL_STAGING = YES
+HUE2MQTT_CFLAGS += -I$(HOST_DIR)/usr/include/boost
 
 $(eval $(meson-package))
